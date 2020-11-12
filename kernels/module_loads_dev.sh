e402b544bc2fa2c0f43e109f0d3e5bf34f9c829f
xzackli <xzackli@gmail.com>
2020-11-12 17:29:44 -0500

    updates to 3.8

--- kernels/module_loads_dev.sh
+++ kernels/module_loads_dev.sh
@@ -2,9 +2,9 @@
 
 module use --append /tigress/zequnl/shared/modulefiles
 module load fftw/gcc/3.3.4 gsl/2.4 anaconda3
-module load healpy/1.13.0
-module load pixell/0.7.0
-module load NaMaster/1.0.1
+module load healpy/1.14.0
+module load pixell/master
+module load NaMaster/master
 module load nawrapper/dev
 
-exec /usr/licensed/anaconda3/2019.10/bin/python -m ipykernel_launcher "$@"
+exec python -m ipykernel_launcher "$@"
